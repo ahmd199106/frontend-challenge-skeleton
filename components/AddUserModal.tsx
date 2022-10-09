81170e2a67298fd2ee4b34526bb67acaef8bc28c
MohammedAK1991 <ahmd199106@gmail.com>
2022-10-09 17:24:07 +0100

    feat(addUserModal_:update the modal to include the user role

--- components/AddUserModal.tsx
+++ components/AddUserModal.tsx
@@ -12,6 +12,7 @@ import {
   FormLabel,
   Flex,
   Text,
+  useToast,
 } from '@chakra-ui/react';
 import React, { Key, useState } from 'react';
 import UsersList from './UsersList';
@@ -22,13 +23,14 @@ function AddUserModal() {
     email: '',
     fullName: '',
   });
+  const [userRoleState, setUserRoleState] = useState('Admin');
   const [usersList, setUsersList] = useState<any>([]);
+  const toast = useToast();
 
   const handleClose = () => {
     setModalOpen(false);
   };
   const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    console.log('onchange');
     setFormState((prev) => ({
       ...prev,
       [event.target.name]: event.target.value,
@@ -37,12 +39,20 @@ function AddUserModal() {
 
   const onSubmit = (event: any) => {
     event.preventDefault();
-    console.log(formState);
+    toast({
+      title: 'Account created.',
+      description: 'Team Member Added Sucessfully.',
+      status: 'success',
+      duration: 5000,
+      isClosable: true,
+      variant: 'left-accent',
+      position: 'top',
+    });
     setUsersList((prev: any) => {
-      //   console.log(prev, 'prev');
-      //   console.log([...prev], '...prev');
-      //   console.log([...prev, formState], '...prev,formsate');
-      return [...prev, { id: Math.random(), ...formState }];
+      return [
+        ...prev,
+        { id: Math.random(), ...formState, role: userRoleState },
+      ];
     });
   };
   console.log(usersList, 'userslist');
@@ -52,15 +62,23 @@ function AddUserModal() {
     setUsersList((prev: any) => {
       return prev.filter((user: { id: number }) => user.id !== id);
     });
-    console.log('yo', id);
+    console.log('Id of the user deleted by the Admin', id);
   };
   console.log(usersList, 'users list after filter');
 
   return (
-    <Flex direction='column' width='400px' height='400px'>
-      <Flex direction='column' width='300px' height='300px'>
-        <Button onClick={() => setModalOpen(true)}>Add new user</Button>
-        <Modal isCentered isOpen={modalOpen} onClose={handleClose} size='xs'>
+    <>
+      <Flex direction='column' width='400px' height='800px'>
+        <Button
+          width='300px'
+          bg='darkblue'
+          color='white'
+          onClick={() => setModalOpen(true)}
+        >
+          Add Team Member
+        </Button>
+        {/* <Flex direction='column' width='300px' height='300px'> */}
+        <Modal isCentered isOpen={modalOpen} onClose={handleClose} size='lg'>
           <ModalOverlay
             bg='none'
             backdropFilter='auto'
@@ -72,63 +90,104 @@ function AddUserModal() {
             flexDirection='column'
             justifyContent='center'
             alignItems='center'
+            width='100%'
+            borderWidth='1px'
           >
-            <ModalHeader>Modal Title</ModalHeader>
+            <ModalHeader>Invite Team Members</ModalHeader>
             <ModalCloseButton />
-            <ModalBody
-              display='flex'
-              flexDirection='column'
-              justifyContent='center'
-              alignItems='center'
-            >
+            <ModalBody display='flex' flexDirection='column' width='100%'>
               <form onSubmit={onSubmit}>
-                <FormControl>
+                <FormControl width='100%'>
                   <FormLabel>Email Address</FormLabel>
                   <Input
+                    width='100%'
+                    required
                     id='email'
                     name='email'
                     type='email'
                     placeholder='First name'
                     onChange={onChange}
+                    value={formState.email}
                   />
                 </FormControl>
 
                 <FormControl mt={4}>
                   <FormLabel>Full name</FormLabel>
                   <Input
+                    required
                     type='text'
                     name='fullName'
                     id='fullName'
                     placeholder='full name'
                     onChange={onChange}
+                    value={formState.fullName}
                   />
+                </FormControl>
+                <FormControl mt={4}>
+                  <FormLabel>Select Role</FormLabel>
+                  <Flex width='100%' flexGrow={1}>
+                    <select
+                      onChange={(event) => setUserRoleState(event.target.value)}
+                      value={userRoleState}
+                    >
+                      <option value='Admin'>Admin</option>
+                      <option value='Viewer'> Viewer</option>
+                    </select>
+                  </Flex>
                 </FormControl>
               </form>
             </ModalBody>
 
-            <ModalFooter>
-              <Button variant='ghost' mr={3} onClick={handleClose}>
+            <ModalFooter width='100%'>
+              <Button
+                variant='outline'
+                mr={3}
+                onClick={handleClose}
+                borderColor='blue'
+              >
                 Cancel
               </Button>
-              <Button colorScheme='blue' onClick={onSubmit}>
+              <Button bg='darkblue' onClick={onSubmit} color='white'>
                 Add Team Member
               </Button>
             </ModalFooter>
           </ModalContent>
         </Modal>
+        {/* </Flex> */}
+        <Flex
+          borderWidth='1px'
+          borderRadius='lg'
+          direction='column'
+          ml='-300px'
+          mr='100px'
+          p='15px'
+        >
+          <Flex width='100%' height='30px' justify='space-between' mb='20px'>
+            <Text p='5px'>Team Member</Text>
+            <Text p='5px' mr='50px'>
+              Role
+            </Text>
+          </Flex>
+          {usersList?.map(
+            (item: {
+              id: number;
+              email: string;
+              fullName: string | undefined;
+              role: string;
+            }) => (
+              <UsersList
+                key={item.id}
+                id={item.id}
+                email={item.email}
+                fullName={item?.fullName}
+                deleteUser={deleteUser}
+                role={item.role}
+              />
+            ),
+          )}
+        </Flex>
       </Flex>
-      {usersList?.map(
-        (item: { id: number; email: string; fullName: string | undefined }) => (
-          <UsersList
-            key={item.id}
-            id={item.id}
-            email={item.email}
-            fullName={item?.fullName}
-            deleteUser={deleteUser}
-          />
-        ),
-      )}
-    </Flex>
+    </>
   );
 }
 
