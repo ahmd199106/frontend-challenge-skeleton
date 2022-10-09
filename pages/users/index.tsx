f1419116c4ddf0478761a4fac90360c81ab2cf04
MohammedAK1991 <ahmd199106@gmail.com>
2022-10-09 17:26:08 +0100

    update the index page of when a user is logged in and fix the styling to match the figma design

--- pages/users/index.tsx
+++ pages/users/index.tsx
@@ -6,6 +6,7 @@ import Header from '../../components/shared/Header'
 import { useRouter } from 'next/router';
 import users from '../../data/users.json';
 import AddUserModal from '../../components/AddUserModal';
+import { Avatar, Button, Flex, Icon, Text } from '@chakra-ui/react';
 
 const Users: NextPage = () => {
   const router = useRouter();
@@ -13,16 +14,27 @@ const Users: NextPage = () => {
   console.log(userId, 'userID1');
   const userIsAuthenticated = users.users.filter((user) => user.id === userId);
   console.log(!!userIsAuthenticated, 'userIsAuthenticated');
+  const logo =
+    userId === '1' ? users.users[0].avatarUrl : users.users[1].avatarUrl;
+  console.log(logo, 'logo');
 
   return (
     <div>
-      <Header />
+      <Header logo={logo} />
       <div className={styles.container}>
         <main className={styles.main}>
-          <h1>Team</h1>
+          <Text fontSize='30px' fontWeight='bold'>
+            Team
+          </Text>
+          <Flex direction='row' justify='space-between'>
+            <Flex flexGrow={1}>
+              <Text pt='10px' color='grey'>
+                Add Team members and assign organisations
+              </Text>
+            </Flex>
+            {userIsAuthenticated && <AddUserModal />}
+          </Flex>
 
-          <p>Place your code here</p>
-          {userIsAuthenticated && <AddUserModal />}
           {!userIsAuthenticated && router.push(`users/noAccess`)}
         </main>
       </div>
