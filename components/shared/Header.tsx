67fea3d114136b7920eea7d9b296fef3c1842eef
MohammedAK1991 <ahmd199106@gmail.com>
2022-10-09 17:21:44 +0100

    feat(Heaader.tsx): update header to receive a fallback avatar logo

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,12 +1,39 @@
 import Image from 'next/image'
 import styles from '../../styles/Header.module.scss'
+import { Avatar, Button, Flex, Icon, Text } from '@chakra-ui/react';
+import { BellIcon, AddIcon, WarningIcon } from '@chakra-ui/icons';
+import router from 'next/router';
 
-const Header = (): JSX.Element => (
-  <div className={styles.header}>
-    <div>
-      <Image src="/logo.svg" alt="Logo" height="32" width="32" />
+
+const Header = ({ logo }): JSX.Element => {
+  const onCLickButton = () => {
+    router.push(`/`);
+  };
+  return (
+    <div className={styles.header}>
+      <div>
+        <Flex direction='row' justify='space-between' align='center'>
+          <Flex>
+            <Image
+              src='/logo.svg'
+              alt='Logo'
+              height='32'
+              width='32'
+              onClick={onCLickButton}
+            />
+            <Text color='white'>Org_name / team</Text>
+          </Flex>
+          <Flex align='center' justify='space-evenly'>
+            <Button bg='darkblue' color='white'>
+              Create new use Case
+            </Button>
+            <BellIcon color='white' w={6} h={6} ml='5px' />
+            <Avatar w={8} h={8} ml='5px' src={logo} pb='3px' />
+          </Flex>
+        </Flex>
+      </div>
     </div>
-  </div>
-)
+  );
+};
 
 export default Header;
