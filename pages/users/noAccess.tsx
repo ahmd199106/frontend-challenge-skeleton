24b97332d20f4ada6456d29198def1ba56ba9d69
MohammedAK1991 <ahmd199106@gmail.com>
2022-10-09 17:25:29 +0100

    update the index pageof the app  with a a no authorised user page and styling

--- pages/users/noAccess.tsx
+++ pages/users/noAccess.tsx
@@ -1,16 +1,27 @@
 import React from 'react';
 import { Text, Button } from '@chakra-ui/react';
 import router from 'next/router';
+import Header from '../../components/shared/Header';
+import styles from '../../styles/Home.module.css';
+import users from '../../data/users.json';
+
 type noAccessProps = {};
 
 const noAccess: React.FC<noAccessProps> = () => {
+  const logo = users.users[1].avatarUrl;
+
   const onCLickButton = () => {
     router.push(`/`);
   };
   return (
-    <div>
-      <Text>No access</Text>
-      <Button onClick={onCLickButton}>Go to main page</Button>
+    <div className={styles.container}>
+      <Header logo={logo} />
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          <Text>No access</Text>
+          <Button onClick={onCLickButton}>Go to main page</Button>
+        </h1>
+      </main>
     </div>
   );
 };
